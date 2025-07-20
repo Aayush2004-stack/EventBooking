14fed64e12dc9be2f4f907df4f1afdbb7879dd41
Aayush <aayushbastola772@gmail.com>
2025-07-20 23:46:00 +0545

    updated variables + new constructor

--- src/model/Event.java
+++ src/model/Event.java
@@ -1,16 +1,34 @@
 package model;
 
 public class Event {
+    private int eventId;
     private String title;
     private double price;
     private int availableTickets;
     private int issuedUserID;
+    private Boolean isApproved;
 
     public Event(String title, double price, int availableTickets, int issuedUserID) {
         this.title = title;
         this.price = price;
         this.availableTickets = availableTickets;
         this.issuedUserID = issuedUserID;
+    }
+
+    public Event(int eventId, String title, double price, int availableTickets, Boolean isApproved ) {
+        this.title = title;
+        this.price = price;
+        this.availableTickets = availableTickets;
+        this.eventId=eventId;
+        this.isApproved=isApproved;
+    }
+
+    public Boolean getIsApproved() {
+        return isApproved;
+    }
+
+    public int getEventId() {
+        return eventId;
     }
 
     public String getTitle() {
